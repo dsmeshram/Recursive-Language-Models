a678ab4e0af7d190fd9073bcdb28c252ab77948c
dsmeshram <damomeshram@gmail.com>
2026-03-28 13:08:01 +0530

    vr update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyrlmlite",
-    version="0.1.1",
+    version="0.1.2",
     author="Damodhar Meshram",
     author_email="damomeshram@gmail.com",
     description="A simple SDK for building recursive language models with OpenAI",
